4a731c7accdbb7bc88ce0a6027b8565a47b133f4
yh1008 <ye.hua@columbia.edu>
2017-01-19 12:56:31 -0500

    aws lambda

---
+++ lambda.js
@@ -0,0 +1,69 @@
+/*
+return episodes or friend recommendations from neo4j
+*/
+"use strict";
+/// <reference path="../typings/index.d.ts" />
+var sdk = require('aws-sdk');
+
+function recommendFriend(email, method, callback) {
+    var httpRequestError = {
+        code: "500",
+        message: "Can't connect neo4j"
+
+    };
+    var internalServerError = {
+        code: "500",
+        message: "neo4j can't connect"
+
+    };
+    /*calling neo4j */
+    var request = require('request');
+    var httpUrlForTransaction = "http://contentcatalog:mL1CSF2PKYTtPkGMdRnv@hobby-hhimaappojekgbkebdhlnhol.dbs.graphenedb.com:24789/db/data/transaction/commit";
+    function runCypherQuery(query, params, callback) {
+        request.post({
+            uri: httpUrlForTransaction,
+            json: { statements: [{ statement: query, parameters: params }] }
+        },
+            function (err, res, body) {
+                callback(err, body);
+            })
+    }
+
+    /**
+     * Let’s fire some queries
+     * */
+    var query = ""
+    if (method === 'episode'){
+        query = "MATCH (me:user {email:{email}})-[myrating:RATE]->(myratedepisode:content)\nMATCH (other:user)-[theirrating:RATE]->(myratedepisode)\nWHERE me <> other\nAND abs(myrating.score - theirrating.score) < 2\nWITH other, myratedepisode\nMATCH (other)-[otherrating:RATE]->(episode:content)\nWHERE myratedepisode <> episode\nWITH avg(otherrating.score) AS avgRating, episode\nRETURN episode\nORDER BY avgRating desc\nLIMIT 3"
+    } else if (method === 'friend') {
+        query = "MATCH  (me:user)-[:FRIEND]-(myFriend:user)-[:FRIEND]-(friendOfFriend:user) \nMATCH (me:user) -[:LIKE] -> (episode:content)\nMATCH (friendOfFriend) - [:LIKE] -> (episode)\nWITH friendOfFriend, count(episode) as nOfCommonLikes, collect(episode) as episodes, me\nWHERE NOT (me)-[:FRIEND]-(friendOfFriend:Person) AND me.email = {email} AND nOfCommonLikes >= 1\nRETURN friendOfFriend as suggestedFriend;"
+    }
+
+    runCypherQuery(
+       query, {
+            email: email,
+        }, function (err, resp) {
+            if (err) {
+                console.log(err);
+            } else {
+                var episodes = "";
+                episodes = resp["results"][0]["data"].map(function(x){return x["row"][0]})
+                callback(null, episodes);
+
+            }
+        }
+    );
+}
+
+function handler(event, context, callback) {
+
+    switch (event.operation) {
+        case 'getRecommendation':
+            var email = event.payload.item.email;
+            var method = event.payload.item.method;
+            recommendFriend(email, method, callback)
+            break;
+
+    }
+}
+exports.handler = handler;
